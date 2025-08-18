430fc0464372907cd17529039df00bde06afe119
NazariiPalahnii <nazariipalahnii@gmail.com>
2025-08-18 11:34:49 +0200

    add assets

--- modules/wm/hyprland/hyprland.nix
+++ modules/wm/hyprland/hyprland.nix
@@ -41,23 +41,23 @@ in
         "$mod, Q, killactive"
         "$mod, W, togglefloating"
         "$mod, G, fullscreen"
-	"$mod, SPACE, exec, rofi -show drun"
+	      "$mod, SPACE, exec, rofi -show drun"
         
         ", Print, exec, hyprshot -m output"
-        "SHIFT, Print, exec, hyprshot -m region"
+       "SHIFT, Print, exec, hyprshot -m region"
 
-	"$mod, 1, workspace, 1"
+	      "$mod, 1, workspace, 1"
         "$mod, 2, workspace, 2"
         "$mod, 3, workspace, 3"
         "$mod, 4, workspace, 4"
         "$mod, 5, workspace, 5"
-        "$mod, 6, workspace, 6"
+        "$mod, 6, workspace, 6" 
         "$mod, 7, workspace, 7"
         "$mod, 8, workspace, 8" 
-	"$mod, 9, workspace, 9"
+	      "$mod, 9, workspace, 9"
         "$mod, 0, workspace, 10"
         "$mod SHIFT, 1, movetoworkspace, 1" 
-	"$mod SHIFT, 2, movetoworkspace, 2"
+	      "$mod SHIFT, 2, movetoworkspace, 2"
         "$mod SHIFT, 3, movetoworkspace, 3"
         "$mod SHIFT, 4, movetoworkspace, 4"
         "$mod SHIFT, 5, movetoworkspace, 5"
@@ -66,7 +66,7 @@ in
         "$mod SHIFT, 8, movetoworkspace, 8"
         "$mod SHIFT, 9, movetoworkspace, 9"
         "$mod SHIFT, 0, movetoworkspace, 10"
-	"$mod, S, togglespecialworkspace, magic"
+	      "$mod, S, togglespecialworkspace, magic"
       	"$mod SHIFT, S, movetoworkspace, special:magic"
       ];
 
@@ -75,7 +75,7 @@ in
         "$mod, mouse:273, resizewindow"
       ];
       exec-once = [
-        "qs ipc call globalIPC toggleLock"
+        #...
       ];
 
       monitor = [
