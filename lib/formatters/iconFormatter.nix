b5eabc85125e7b914063215af67dc36118bd970f
NazariiPalahnii <nazariipalahnii@gmail.com>
2026-01-19 00:30:23 +0100

    t

--- lib/formatters/iconFormatter.nix
+++
@@ -1,8 +0,0 @@
-{ ... }:
-{
-  # Get icon without formatting
-  getIcon = iconPath: fallback: iconPath.char or fallback;
-
-  # Format icon with trailing space for starship/terminal use
-  formatIcon = iconPath: fallback: "${iconPath.char or fallback} ";
-}
