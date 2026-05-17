63549d246c915d9e51fce3930e170236bf584ea9
yurei07 <125127108+yurei07@users.noreply.github.com>
2026-05-17 21:00:31 +0200

    feat: create iso 8

--- hosts/iso/iso.nix
+++ hosts/iso/iso.nix
@@ -52,15 +52,12 @@
       "https://cache.nixos.org"
       "https://nix-community.cachix.org"
       "https://cache.garnix.io"
-      "https://zen-browser.cachix.org"
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCUSDs="
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-      "zen-browser.cachix.org-1:ePDN0rkGMKQBBkDMBNJBNmRR5jCH3cOWYL6VEwCN5E="
     ];
   };
-
   system.stateVersion = "25.05";
 }
